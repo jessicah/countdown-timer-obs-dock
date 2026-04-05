c4e4edd25237d34a150a787f9d60bee91afb4a9a
Jessica Hamilton <jessica.l.hamilton@gmail.com>
2026-04-05 13:33:10 +1200

    initial: copy of github.com/obsproject/obs-plugintemplate

---
+++ src/plugin-main.cpp
@@ -0,0 +1,49 @@
+/*
+Countdown Timer
+Copyright (C) 2026 Jessica Hamilton
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/>
+*/
+
+#include <memory>
+
+#include <QVersionNumber>
+
+#include <obs-config.h>
+#include <obs-module.h>
+#include <plugin-support.h>
+
+#include "countdown-timer-dock.hpp"
+
+OBS_DECLARE_MODULE()
+OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
+
+bool obs_module_load(void)
+{
+    if (obs_get_version() < MAKE_SEMANTIC_VERSION(31, 0, 0)) {
+        obs_log(LOG_WARNING, "This plugin requires OBS Studio 31.0.0 or later.");
+        return false;
+    }
+
+    obs_log(LOG_INFO, "Plugin version: %s", PLUGIN_VERSION);
+    obs_log(LOG_INFO, "OBS version: %d.%d.%d (compiled), %s (runtime)", LIBOBS_API_MAJOR_VER, LIBOBS_API_MINOR_VER,
+            LIBOBS_API_PATCH_VER, obs_get_version_string());
+    obs_log(LOG_INFO, "Qt version: %s (compiled), %s (runtime)", QT_VERSION_STR, qVersion());
+
+	return true;
+}
+
+void obs_module_unload(void)
+{
+}
